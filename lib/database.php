dbce4fceca66236bd0b38a8e84664d647775b4ce
Shuvo575 <sohag.web.dev@gmail.com>
2019-10-24 10:39:10 +0600

    database file edited

--- lib/database.php
+++ lib/database.php
@@ -13,31 +13,3 @@ function db_connection()
 	return $conn;
 }
 
-
-function get_tb($sql, $count = false)
-{
-	if (db_connection() == false) {
-		return null;
-	}
-	$conn = db_connection();
-	$result = mysqli_query($conn, $sql);
-	// return $sql;
-	if (!$result)
-		return false;
-
-	if (mysqli_num_rows($result) > 0) {
-		$cnt = 0;
-		// output data of each row
-		while ($row = mysqli_fetch_assoc($result)) {
-			$table[] = $row;
-			$cnt++;
-		}
-		if ($count)
-			return [$table, $cnt];
-		else
-			return $table;
-	} else
-		return false;
-
-	mysqli_close($conn);
-}
