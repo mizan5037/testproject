71f1f9bdfb7b4364c96f02353ebd972d5619bc6f
Mizanur Rahman <mrahman5037@gmail.com>
2019-10-29 16:56:15 +0600

    stationary

---
+++ controller/add_new_stationary.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['item_name']) && $_POST['specification'] != '' && isset($_POST['unit']) && isset($_POST['unit'])) {
+    $conn = db_connection();
+    $name = $_POST['item_name'];
+    $specification = $_POST['specification'];
+    $unit = $_POST['unit'];
+    $user_id = get_ses('user_id');
+
+    $sql = "INSERT INTO stationary_item (Name,Description,MeasurmentUnit,AddedBy)
+
+		   		values('$name','$specification','$unit','$user_id')";
+
+    if (mysqli_query($conn, $sql)) {
+        notice('success', 'New Stationary Item added Successfully');
+    } else {
+        notice('error', $sql . "<br>" . mysqli_error($conn));
+    }
+}
