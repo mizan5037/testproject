a4fb3679e21414c9c27713c5459e6b3bbde7be72
Sohag Hasan <shohaghasan575@gmail.com>
2019-11-07 10:35:25 +0600

    PDF report template added

---
+++ report.template.php
@@ -0,0 +1,13 @@
+<?php
+//Main Page
+require_once 'config.php';
+require_once 'lib/session.php';
+require_once 'lib/database.php';
+//linking Functions
+require_once 'lib/functions.php';
+
+require_once 'lib/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1 style="color:red">Hello Shuvo!</h1>');
+$mpdf->Output();
