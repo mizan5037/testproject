cb18f32e317806f5abf98e6cbe7f0aaa618d13d5
mrzrashed <mrzrashed01@gmail.com>
2019-11-07 16:12:09 +0600

    Hourly Form done

--- controller/add_hourly_form.php
+++ controller/add_hourly_form.php
@@ -1,20 +1,14 @@
 <?php
+$hourColum = array(9 => 'nine', 10 => 'ten', 11 => 'eleven', 12 => 'twelve', 1 => 'one', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight');
 $conn = db_connection();
-if (isset($_POST['date']) && isset($_POST['floorno']) && isset($_POST['line']) && isset($_POST['po']) && isset($_POST['style']) && isset($_POST['color']) && isset($_POST['hour']) && isset($_POST['quantity'])  ) {
+
+if (isset($_POST['date']) && isset($_POST['floor']) && isset($_POST['line']) && isset($_POST['po']) && isset($_POST['style']) && isset($_POST['color']) && isset($_POST['hour']) && isset($_POST['quantity'])  ) {
 
     $date = $_POST['date'];
-    $floorno = $_POST['floorno'];
+    $floorno = $_POST['floor'];
     $user_id = get_ses('user_id');
 
-    $sql = "INSERT INTO hourly_production (Date,FloorNO,AddedBy)
-	   values('$date','$floorno','$user_id')";
 
-    if (mysqli_query($conn, $sql)) {
-        notice('success', 'New Hourly Production added Successfully');
-        $last_id = mysqli_insert_id($conn);
-    } else {
-        notice('error', $sql . "<br>" . mysqli_error($conn));
-    }
 
     //hourly production datails
     $line = $_POST['line'];
@@ -24,26 +18,50 @@ if (isset($_POST['date']) && isset($_POST['floorno']) && isset($_POST['line']) &
     $hour = $_POST['hour'];
     $quantity = $_POST['quantity'];
 
-    $lineCount = "SELECT * FROM hourly_production_details WHERE LineNo = '$line'";
-    $lineCount = count(mysqli_query($conn, $lineCount));
+
     for ($i = 0; $i < sizeof($color); $i++) {
-        if($lineCount <= 0 ){
-          $sql = "INSERT INTO hourly_production_details (HourlyProductionID,LineNo,POID,StyleID,Color,AddedBy) WHERE LineNo = $line[$i]
-          values('$last_id','$line[$i]','$po[$i]','$style[$i]','$color[$i]','$user_id') ";
+
+      $lineCount = "SELECT p.*, d.* FROM hourly_production_details d LEFT JOIN hourly_production p ON p.HourlyProductionID = d.HourlyProductionID WHERE  p.Date = '$date' AND p.FloorNO = '$floorno' AND d.LineNo = '$line[$i]' AND d.POID = '$po[$i]' AND d.StyleID='$style[$i]' AND d.Color = '$color[$i]'";
+
+
+      $hourlyId = mysqli_fetch_assoc(mysqli_query($conn,$lineCount))['ID'];
+
+      $lineCount = mysqli_num_rows(mysqli_query($conn, $lineCount));
+
+      if($lineCount < 1 ){
+
+        $sql = "INSERT INTO hourly_production (Date,FloorNO,AddedBy)
+         values('$date','$floorno','$user_id')";
+
+        if (mysqli_query($conn, $sql)) {
+            notice('success', 'New Hourly Production added Successfully');
+            $last_id = mysqli_insert_id($conn);
+        } else {
+            notice('error', $sql . "<br>" . mysqli_error($conn));
+        }
+
+          $hou = $hourColum[$hour[$i]];
+          $sql = "INSERT INTO hourly_production_details (HourlyProductionID,LineNo,POID,StyleID,Color,$hou,AddedBy) values ('$last_id','$line[$i]','$po[$i]','$style[$i]','$color[$i]','$quantity[$i]','$user_id') ";
 
           if (mysqli_query($conn, $sql)) {
-              notice('success', 'New Hourly Production added Successfully');
-              if($hour[$i] == '9'){
-                $sql = "UPDATE `hourly_production_details` SET `nine`='$hour[$i]' WHERE LineNo = $line[$i]";
-              }
+              notice('success', 'New Quantity added Successfully');
+              // $sql = "UPDATE `hourly_production_details` SET $hou='$hour[$i]' WHERE LineNo = $line[$i]";
           } else {
               notice('error', $sql . "<br>" . mysqli_error($conn));
           }
         }
         else {
+          $hou = $hourColum[$hour[$i]];
 
+          $sql = "UPDATE `hourly_production_details` SET $hou='$quantity[$i]' WHERE ID = $hourlyId";
+
+          if (mysqli_query($conn, $sql)) {
+              notice('success', 'New Quantity Updated Successfully');
+          } else {
+              notice('error', $sql . "<br>" . mysqli_error($conn));
+          }
         }
 
     }
-
+    nowgo('/index.php?page=all_hourly_form');
 }
--- views/hourly_form.php
+++ views/hourly_form.php
@@ -1,6 +1,7 @@
 <?php
 
 $PageTitle = "Hourly Production Form | Optima Inventory";
+$conn = db_connection();
 function customPageHeader()
 {
     ?>
@@ -31,31 +32,27 @@ include_once "includes/header.php";
         <div class="card-body">
             <form class="needs-validation" method="POST"novalidate>
                 <div class="form-row">
-                    <div class="col-md-4 mb-3">
+                    <div class="col-md-4 mb-4">
                         <label for="validationTooltip01">Date</label>
-                        <input type="date" class="form-control" name="date" id="validationTooltip01" placeholder="Date" required>
+                        <input type="date" class="form-control form-control-sm" name="date" id="validationTooltip01" placeholder="Date" required>
                     </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="validationTooltip02">Floor no</label>
-                        <input type="text" name="floorno" class="form-control" id="validationTooltip02" placeholder="Floor no" required>
+
+                    <div class="col-md-4 mb-4">
+                        <label for="validationTooltip02">Floor No.</label>
+                        <select name="floor" class="po  form-control form-control-sm" required>
+                          <option>Choose </option>
+                          <?php
+                          $sql = "SELECT * FROM floor WHERE status = 1";
+                          $results = mysqli_query($conn, $sql);
+                          while ($result = mysqli_fetch_assoc($results)) {
+                              echo '<option value="' . $result['floor_id'] . '">' . $result['floor_name'] . '</option>';
+                          }
+                          ?>
+                        </select>
                     </div>
 
                 </div>
-                <style>
-                    #mytable>tbody>tr>td {
-                        padding: 0px;
-                        margin: 0px;
-                        margin-bottom: 0px !important;
-                    }
 
-                    #mytable>tbody>tr>td>input {
-                        width: 100%;
-                    }
-
-                    #mytable>tbody>tr>td>input[type=text] {
-                        width: 100px;
-                    }
-                </style>
                 <div class="form-row">
                     <table class="mb-0 table table-bordered table-hover order-list" id="mytable">
                         <thead>
@@ -74,10 +71,9 @@ include_once "includes/header.php";
                             <tr>
                                 <th scope="row">1</th>
                                 <td>
-                                    <select name="line[]" class="po  form-control" required>
+                                    <select name="line[]" class="po  form-control form-control-sm" required>
                                         <option></option>
                                         <?php
-                                        $conn = db_connection();
                                         $sql = "SELECT * FROM line WHERE status = 1";
                                         $results = mysqli_query($conn, $sql);
                                         while ($result = mysqli_fetch_assoc($results)) {
@@ -87,7 +83,7 @@ include_once "includes/header.php";
                                     </select>
                                 </td>
                                 <td>
-                                    <select name="po[]" class="po  form-control" required>
+                                    <select name="po[]" class="po  form-control form-control-sm" required>
                                         <option></option>
                                         <?php
                                         $conn = db_connection();
@@ -100,7 +96,7 @@ include_once "includes/header.php";
                                     </select>
                                 </td>
                                 <td>
-                                    <select name="style[]" class="style  form-control" required>
+                                    <select name="style[]" class="style  form-control form-control-sm" required>
                                         <option></option>
                                         <?php
                                         $conn = db_connection();
@@ -113,7 +109,7 @@ include_once "includes/header.php";
                                     </select>
                                 </td>
                                 <td>
-                                    <select name="color[]" class="color  form-control" required>
+                                    <select name="color[]" class="color  form-control form-control-sm" required>
                                         <option></option>
                                         <?php
                                         $conn = db_connection();
@@ -126,7 +122,7 @@ include_once "includes/header.php";
                                     </select>
                                 </td>
                                 <td>
-                                    <select name="hour[]" class="form-control" required>
+                                    <select name="hour[]" class="form-control form-control-sm" required>
                                         <option >Choose Hour</option>
                                         <option value="9">9</option>
                                         <option value="10">10</option>
@@ -141,7 +137,7 @@ include_once "includes/header.php";
                                         <option value=8>8</option>
                                     </select>
                                 </td>
-                                <td><input placeholder="Quantity" name="quantity[]" type="number" class="form-control"></td>
+                                <td><input placeholder="Quantity" name="quantity[]" type="number" class="form-control form-control-sm"></td>
                                 <td><a class="deleteRow"></a></td>
                             </tr>
 
@@ -182,7 +178,7 @@ function customPagefooter()
                 var cols = "";
 
                 cols += '<th scope="row">' + counter + '</th>';
-                cols += '<td><select name="line[]" class="po form-control" required><option></option><?php
+                cols += '<td><select name="line[]" class="po form-control form-control-sm" required><option></option><?php
                     $conn = db_connection();
                     $sql = "SELECT * FROM line WHERE status = 1";
                     $results = mysqli_query($conn, $sql);
@@ -190,7 +186,7 @@ function customPagefooter()
                         echo '<option value="' . $result['id'] . '">' . $result['line'] . '</option>';
                     }
                     ?></select></td>';
-                cols += '<td><select name="po[]" class="po form-control" required><option></option>';
+                cols += '<td><select name="po[]" class="po form-control form-control-sm" required><option></option>';
                 <?php
                     $conn = db_connection();
                     $sql = "SELECT * FROM po WHERE status = 1";
@@ -200,7 +196,7 @@ function customPagefooter()
                     }
                     ?>
                 cols += '</select></td>';
-                cols += '<td><select name="style[]" class="style  form-control" required><option></option>';
+                cols += '<td><select name="style[]" class="style  form-control form-control-sm" required><option></option>';
                 <?php
                     $conn = db_connection();
                     $sql = "SELECT * FROM style WHERE status = 1";
@@ -210,7 +206,7 @@ function customPagefooter()
                     }
                     ?>
                 cols += '</select></td>';
-                cols += '<td><select name="color[]" class="color form-control" required><option></option>';
+                cols += '<td><select name="color[]" class="color form-control form-control-sm" required><option></option>';
                 <?php
                     $conn = db_connection();
                     $sql = "SELECT * FROM color WHERE status = 1";
@@ -220,8 +216,8 @@ function customPagefooter()
                     }
                     ?>
                 cols += '</select></td>';
-                cols += '<td><select name="hour[]" class="form-control" required><option >Choose Hour</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="1">1</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value=8>8</option></select></td>';
-                cols += '<td><input placeholder="Quantity" name="quantity[]" type="number" class="form-control"></td>';
+                cols += '<td><select name="hour[]" class="form-control form-control-sm" required><option >Choose Hour</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="1">1</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value=8>8</option></select></td>';
+                cols += '<td><input placeholder="Quantity" name="quantity[]" type="number" class="form-control form-control-sm"></td>';
                 cols += '<td><input type="button" class="ibtnDel btn btn-sm btn-danger "  value="Delete"></td>';
                 newRow.append(cols);
                 $("table.order-list").append(newRow);
