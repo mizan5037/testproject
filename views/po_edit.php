efc8515eb4a67705614e575f95c34c90fdf688f4
Sohag Hasan <shohaghasan575@gmail.com>
2019-11-14 12:48:04 +0600

    PO bug fixed

--- views/po_edit.php
+++ views/po_edit.php
@@ -85,11 +85,11 @@ include_once "includes/header.php";
                     </div>
                     <div class="col-md-3 mb-3">
                         <label for="validationTooltip01">Final Destination</label>
-                        <input type="Text" name="final_destination" value="<?= $single_po['POFinalDestination'] ?>" class="form-control" id="validationTooltip01" placeholder="Final Destination" required>
+                        <input type="Text" name="final_destination" value="<?= $single_po['POFinalDestination'] ?>" class="form-control" id="validationTooltip01" placeholder="Final Destination">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="validationTooltip03">Special Instruction</label>
-                        <textarea type="number" name="special_instruction" class="form-control" id="validationTooltip03" placeholder="Special Instruction" rows="1" required><?= $single_po['POSpecialInstruction'] ?></textarea>
+                        <textarea type="number" name="special_instruction" class="form-control" id="validationTooltip03" placeholder="Special Instruction" rows="1"><?= $single_po['POSpecialInstruction'] ?></textarea>
                     </div>
                 </div>
                 <div class="form-row">
@@ -119,7 +119,10 @@ include_once "includes/header.php";
 
                                     ?>
                                     <tr>
-                                        <th scope="row"><?= $counts++ ?></th>
+                                        <th scope="row">
+                                            <?= $counts++ ?>
+                                            <input type="hidden" value="<?= $row['OrderdescriptionID'] ?>" name="OrderdescriptionID[]">
+                                        </th>
                                         <td>
                                             <select name="style[]" class="style mb-2 form-control-sm form-control" required>
                                                 <?php
@@ -141,7 +144,7 @@ include_once "includes/header.php";
                                             <input placeholder="Color" type="text" value="<?= $row['Color'] ?>" name="color[]" class="mb-2 form-control-sm form-control" required>
                                         </td>
                                         <td>
-                                            <input placeholder="CLR No" type="text" value="<?= $row['ClrNo'] ?>" name="clr_no[]" class="mb-2 form-control-sm form-control" required>
+                                            <input placeholder="CLR No" type="text" value="<?= $row['ClrNo'] ?>" name="clr_no[]" class="mb-2 form-control-sm form-control">
                                         </td>
                                         <td>
                                             <input placeholder="DZS" type="number" name="dzs[]" value="<?= $row['Dzs'] ?>" class="mb-2 form-control-sm form-control">
@@ -184,7 +187,10 @@ include_once "includes/header.php";
 
                                     ?>
                                     <tr>
-                                        <th scope="row"><?= $count++ ?></th>
+                                        <th scope="row">
+                                            <?= $count++ ?>
+                                            <input type="hidden" value="<?= $row['PrePackID'] ?>" name="PrePackID[]">
+                                        </th>
                                         <td>
                                             <input placeholder="Size" type="text" name="size[]" value="<?= $row['PrePackSize'] ?>" class="mb-2 form-control-sm form-control" required>
                                         </td>
@@ -219,174 +225,7 @@ include_once "includes/header.php";
 function customPagefooter()
 {
     ?>
-    <script>
-        //table top
-        $(document).ready(function() {
-            var counter1 = 0;
-            var limit1 = 100;
 
-            $("#addrow1").on("click", function() {
-                //alert('clicked');
-
-                counter1 = $('#myTable1 tr').length - 3;
-                console.log(counter1);
-
-                var newRow1 = $("<tr>");
-                var cols1 = "";
-
-                cols1 += '<th scope="row">' + counter1 + '</th>';
-                cols1 += '<td><select name="style[]" class="style mb-2 form-control-sm form-control" required><option></option>';
-                <?php
-                    $conn = db_connection();
-                    $sql = "SELECT * FROM style WHERE status = 1";
-                    $results = mysqli_query($conn, $sql);
-                    while ($result = mysqli_fetch_assoc($results)) {
-                        echo 'cols1 += \'<option value="' . $result['StyleID'] . '">' . $result['StyleNumber'] . '</option>\'; ';
-                    }
-                    ?>
-                cols1 += '</select></td>';
-                cols1 += '<td><input type="text" placeholder="Color" class="mb-2 form-control-sm form-control" name="color' + counter1 + '" required/></td>';
-                cols1 += '<td><input type="text" placeholder="CLR No" class="mb-2 form-control-sm form-control" name="clrno' + counter1 + '" required/></td>';
-                cols1 += '<td><input type="number" placeholder="DZS" class="mb-2 form-control-sm form-control" name="dzs' + counter1 + '"/></td>';
-                cols1 += '<td><input type="number" placeholder="P/Pack" class="mb-2 form-control-sm form-control" name="ppack' + counter1 + '"/></td>';
-                cols1 += '<td><input type="number" placeholder="Units" class="mb-2 form-control-sm form-control" name="units' + counter1 + '"/></td>';
-
-                cols1 += '<td><input type="button" class="ibtnDel1 btn btn-sm btn-warning"  value="Delete"></td>';
-                newRow1.append(cols1);
-                if (counter1 >= limit1) $('#addrow1').attr('disabled', true).prop('value', "You've reached the limit");
-                $("table.order-list1").append(newRow1);
-                counter1++;
-            });
-
-            $("table.order-list1").on("change", 'input[name^="dzs"]', function(event) {
-                calculateDZSTotal();
-            });
-            $("table.order-list1").on("change", 'input[name^="ppack"]', function(event) {
-                calculatePPackTotal();
-            });
-            $("table.order-list1").on("change", 'input[name^="units"]', function(event) {
-                calculateUnitsTotal();
-            });
-
-
-            $("table.order-list1").on("click", ".ibtnDel1", function(event) {
-                $(this).closest("tr").remove();
-                calculateDZSTotal();
-                calculatePPackTotal();
-                calculateUnitsTotal();
-
-                counter1 -= 1
-                $('#addrow1').attr('disabled', false).prop('value', "Add Row");
-            });
-
-
-        });
-
-
-        function calculateDZSTotal() {
-            var grandDZS = 0;
-            $("table.order-list1").find('input[name^="dzs"]').each(function() {
-                grandDZS += +$(this).val();
-            });
-            $("#totalDZS").text(grandDZS.toFixed(0));
-        }
-
-        function calculatePPackTotal() {
-            var grandPPack = 0;
-            $("table.order-list1").find('input[name^="ppack"]').each(function() {
-                grandPPack += +$(this).val();
-            });
-            $("#totalPpack").text(grandPPack.toFixed(0));
-        }
-
-        function calculateUnitsTotal() {
-            var grandUnits = 0;
-            $("table.order-list1").find('input[name^="units"]').each(function() {
-                grandUnits += +$(this).val();
-            });
-            $("#totalUnits").text(grandUnits.toFixed(0));
-        }
-
-
-        //table bottom
-        $(document).ready(function() {
-            var counter = 0;
-            var limit = 100;
-
-            $("#addrow").on("click", function() {
-
-                counter = $('#myTable tr').length - 3;
-
-                var newRow = $("<tr>");
-                var cols = "";
-
-                cols += '<th scope="row">' + counter + '</th>';
-                cols += '<td><input type="text" placeholder="Size" class="mb-2 form-control-sm form-control" name="size' + counter + '" required/></td>';
-                cols += '<td><input type="text" placeholder="PrePack Code" class="mb-2 form-control-sm form-control" name="color' + counter + '" required/></td>';
-                cols += '<td><input type="number" placeholder="Qty" class="mb-2 form-control-sm form-control" name="qty' + counter + '"/></td>';
-
-                cols += '<td><input type="button" class="ibtnDel btn btn-sm btn-warning"  value="Delete"></td>';
-                newRow.append(cols);
-                if (counter >= limit) $('#addrow').attr('disabled', true).prop('value', "You've reached the limit");
-                $("table.order-list").append(newRow);
-                counter++;
-            });
-
-            $("table.order-list").on("change", 'input[name^="qty"]', function(event) {
-                calculateRow($(this).closest("tr"));
-                calculateGrandTotal();
-            });
-
-
-            $("table.order-list").on("click", ".ibtnDel", function(event) {
-                $(this).closest("tr").remove();
-                calculateGrandTotal();
-
-                counter -= 1
-                $('#addrow').attr('disabled', false).prop('value', "Add Row");
-            });
-
-
-        });
-
-
-
-        function calculateRow(row) {
-            var price = +row.find('input[name^="qty"]').val();
-
-        }
-
-        function calculateGrandTotal() {
-            var grandTotal = 0;
-            $("table.order-list").find('input[name^="qty"]').each(function() {
-                grandTotal += +$(this).val();
-            });
-            $("#grandtotal").text('Total Qty: ' + grandTotal.toFixed(0));
-        }
-
-        $("#reset").on("click", function() {
-            $("#grandtotal").text('');
-        });
-
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function() {
-            'use strict';
-            window.addEventListener('load', function() {
-                // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                var forms = document.getElementsByClassName('needs-validation');
-                // Loop over them and prevent submission
-                var validation = Array.prototype.filter.call(forms, function(form) {
-                    form.addEventListener('submit', function(event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
-                        form.classList.add('was-validated');
-                    }, false);
-                });
-            }, false);
-        })();
-    </script>
 <?php }
 include_once "includes/footer.php";
 ?>
