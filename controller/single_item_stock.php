38b3f7ce0a7bca37cb264c556363015d3d7e2eea
Sohag Hasan <shohaghasan575@gmail.com>
2019-12-19 13:46:51 +0600

    simple bug fixed

--- controller/single_item_stock.php
+++ controller/single_item_stock.php
@@ -23,7 +23,7 @@ if (
     $sqlissue   = "SELECT DISTINCT ir.*, i.ItemName, s.size, i.ItemMeasurementUnit FROM item_issue_access ir LEFT JOIN order_description od ON ir.POID = od.POID LEFT JOIN masterlc_description md ON md.POID = od.POID LEFT JOIN masterlc m ON m.MasterLCID = md.MasterLCID LEFT JOIN item i ON i.ItemID = ir.ItemID LEFT JOIN size s ON s.id = ir.Size WHERE m.MasterLCBuyer = '$buyer' AND ir.Color = '$color' AND ir.StyleID = '$style' AND ir.POID = '$po'";
     $queryissue = mysqli_query($conn, $sqlissue);
 
-    $sqlttl = "SELECT b.BuyerID, b.BuyerName, c.id, c.color, s.StyleID, s.StyleNumber, p.POID, p.PONumber FROM buyer b , color c, style s, po p WHERE b.BuyerID = 1 AND c.id = 1 AND s.StyleID = 1 AND p.POID = 1";
+    $sqlttl = "SELECT b.BuyerID, b.BuyerName, c.id, c.color, s.StyleID, s.StyleNumber, p.POID, p.PONumber FROM buyer b , color c, style s, po p WHERE b.BuyerID = $buyer AND c.id = $color AND s.StyleID = $style AND p.POID = $po";
     $rowttl = mysqli_fetch_assoc(mysqli_query($conn, $sqlttl));
 } else {
     nowgo('/index.php?page=item_stock');
